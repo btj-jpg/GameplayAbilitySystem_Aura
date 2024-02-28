a96ca895e2af9730dc574339345117d906659a69
btj-jpg <micchan9190@gmail.com>
2024-02-28 21:52:05 +0900

    first commit

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,65 @@
+// Copyright Druid Mechanics
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	// レプリケーションする
+	// クライアント　＝　サーバーに接続している側（プレイヤーなど）
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	// 引数2 優先度
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// マウス設定
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// マウスがビューポートにロックされなくなる
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVecter = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation =  GetControlRotation();
+	FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ConttollePawn = GetPawn<APawn>())
+	{
+		ConttollePawn->AddMovementInput(ForwardDirection, InputAxisVecter.Y);
+		ConttollePawn->AddMovementInput(RightDirection, InputAxisVecter.X);
+	}
+}
